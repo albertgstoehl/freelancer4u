a22377d1d51220a27cc3a94cd98eb620d93a8ca9
Albert Gstoehl <albert@gstoehl.dev>
2025-02-26 09:29:58 +0100

    Implemented get endpoint for company by id. Closes #4

--- src/main/java/ch/zhaw/freelancer4u/controller/CompanyController.java
+++ src/main/java/ch/zhaw/freelancer4u/controller/CompanyController.java
@@ -1,11 +1,13 @@
 package ch.zhaw.freelancer4u.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,15 @@ public class CompanyController {
     public ResponseEntity<List<Company>> getCompanies() {
         List<Company> companies = companyRepository.findAll();
         return new ResponseEntity<>(companies, HttpStatus.OK);
+    }
+
+    @GetMapping("/company/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable String id) {
+        Optional<Company> company = companyRepository.findById(id);
+        if (company.isPresent()) {
+            return new ResponseEntity<>(company.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
